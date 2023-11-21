0191a328a8f9f1fd608349f5660bdebcee0c79d5
steven32323 <107620513+steven32323@users.noreply.github.com>
2023-11-21 13:59:47 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,98 @@
+const fs = require("fs");
+const http = require("http");
+const url = require("url");
+
+const replaceTemplate = require("./modules/replaceTemplate");
+
+////////////////////////////////////////////////////////////////////////
+// FILES
+
+// Blocking, synchronous way
+// const textIn = fs.readFileSync("./txt/input.txt", "utf8");
+
+// const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
+
+// fs.writeFileSync("./txt/output.txt", textOut);
+
+// console.log("File has been written.");
+//
+
+// Non-blocking, asynchronous way
+// fs.readFile("./txt/start.txt", "utf8", (err, data1) => {
+//   if (err) return console.error("ERROR! 💥💥");
+
+//   fs.readFile(`./txt/${data1}.txt`, "utf8", (err, data2) => {
+//     console.log(data2);
+//     fs.readFile("./txt/append.txt", "utf8", (err, data3) => {
+//       console.log(data3);
+
+//       fs.writeFile(
+//         "./txt/final.txt",
+//         `${data2}\n${data3}`,
+//         "utf8",
+//         (err, data4) => {
+//           console.log("Your file has been written successfully");
+//         }
+//       );
+//     });
+//   });
+// });
+
+///////////////////////////////////////////
+// SERVER
+
+// synchronous code, but it only happens once
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  "utf8"
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf8"
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  "utf8"
+);
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf8");
+const dataObj = JSON.parse(data);
+
+// async code
+const server = http.createServer((req, res) => {
+  const { query, pathname } = url.parse(req.url, true);
+
+  // OVERVIEW PAGE
+  if (pathname === "/" || pathname === "/overview") {
+    res.writeHead(200, { "Content-type": "text/html" });
+
+    const cardsHtml = dataObj.map((e) => replaceTemplate(tempCard, e)).join("");
+    const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardsHtml);
+
+    res.end(output);
+
+    // PRODUCT PAGE
+  } else if (pathname === "/product") {
+    res.writeHead(200, { "Content-type": "text/html" });
+    const product = dataObj[query.id];
+    const output = replaceTemplate(tempProduct, product);
+
+    res.end(output);
+
+    // API PAGE
+  } else if (pathname === "/api") {
+    res.writeHead(200, { "Content-type": "application/json" });
+    res.end(data);
+
+    // NOT FOUND
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+      "my-own-header": "hello-world",
+    });
+    res.end("<h1>This page could not be found</h1>");
+  }
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening to requests on port 8000");
+});
